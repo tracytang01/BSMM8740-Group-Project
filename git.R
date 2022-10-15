511aa86ed40c669cdc31a8a2e90f1c2f4ddd48b5
Shikai (Zoe) Jin <79543449+shikaijin@users.noreply.github.com>
2022-10-15 17:19:41 -0400

    Create git.R

---
+++ git.R
@@ -0,0 +1,5 @@
+# Create a new branch. Replace <BRANCH-NAME> with a descriptive name, for example speed-and-distance-report:
+git branch <BRANCH-NAME>
+
+# Check out to your new branch:
+git checkout <BRANCH-NAME>
